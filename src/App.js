78b1481b3c5b93d9ff071ac10cedade4c3470607
hollyallison <145831646+hollyallison@users.noreply.github.com>
2023-12-31 16:41:31 +0000

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">Weather App</div>
-  );
+export default function App() {
+  return ( <div className="App"> Weather App
+ <footer> 
+  This project was coded by Holly Allison and is 
+  <a href="https://github.com/hollyallison/react-weather" target="_blank"> open-sourced </a>on Github
+  </footer>
+
+</div>  );
 }
 
-export default App;
+ 
