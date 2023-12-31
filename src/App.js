19eec52a07fd798f4e524e31f3648d9bfc3a9871
hollyallison <145831646+hollyallison@users.noreply.github.com>
2023-12-31 20:06:47 +0000

    basic css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Weather from './weather.js';
 
 export default function App() {
   return ( <div className="App"> 
-  <h1>Weather App</h1>
+  <div className="container">
+
+  < Weather />
  <footer> 
   This project was coded by Holly Allison and is 
   <a href="https://github.com/hollyallison/react-weather" target="_blank"> open-sourced </a>on Github
   </footer>
 
+</div>
 </div>  );
 }
 
