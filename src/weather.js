6654ce455ee9304f1b85717efd7a1096ad29d4bd
hollyallison <145831646+hollyallison@users.noreply.github.com>
2023-12-31 21:43:02 +0000

    updated

--- src/weather.js
+++ src/weather.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
     const[weatherData, setWeatherdata] = useState({ ready: false });
     function handleResponse(response) {
         console.log(response.data);
         setWeatherdata({
-         ready: true,
+          ready: true,
           city: response.data.city,
           description: response.data.condition.description,
           date: "Wednesday 07:00",
@@ -16,9 +16,8 @@ export default function Weather() {
           wind: response.data.wind.speed,
           icon: response.data.condition.icon_url,
         });
-    
       }
-      
+    
 
 if (weatherData.ready) {
  return (
@@ -39,8 +38,7 @@ if (weatherData.ready) {
         <div className="col-6">
           <img
             src={weatherData.icon}
-            alt="{weatherData.description}" className="float-left"
-          />
+            alt="{weatherData.description}" className="float-left" />
          <div className="float-left">
             <span className="temperature">{Math.round(weatherData.temperature)}</span>
             <span className="unit"> ℃</span>
@@ -57,8 +55,7 @@ if (weatherData.ready) {
   );   
 } else { 
 const apiKey = "2c40a31a9bebb30oc02aftf7a42a8b2e";
-let city = "Glasgow";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;    
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;    
 axios.get(apiUrl).then(handleResponse);
 
 return "loading...";
