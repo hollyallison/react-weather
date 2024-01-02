66d82e284872d5010a374d0b1e4052a9d50d9fef
hollyallison <145831646+hollyallison@users.noreply.github.com>
2024-01-02 15:07:15 +0000

    Revert "add plug in for netlify"
    This reverts commit 8078f2e0019f762c6cfb0ae33a14d6369c927739.

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -13,11 +13,10 @@ export default function WeatherForecastDay(props) {
 
   function day() {
     let date = new Date(props.data.time * 1000);
-    date.setDate(date.getDate() + 1);
     let day = date.getDay();
-  
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  
+
+    let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+
     return days[day];
   }
 
