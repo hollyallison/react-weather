91808d3ee31bf42f719f7d65b1692aee2a09bd3d
hollyallison <145831646+hollyallison@users.noreply.github.com>
2024-01-03 07:05:20 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   < Weather  defaultCity="Glasgow"/>
  <footer> 
   This project was coded by Holly Allison and is 
-  <a href="https://github.com/hollyallison/react-weather" target="_blank"> open-sourced </a>on Github
+  <a rel='noreferrer' href="https://github.com/hollyallison/react-weather" target="_blank"> open-sourced </a>on Github
   </footer>
 
 </div>
